174cdb7b649302feed95d5e5dbaba918eefd3e37
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2021-08-03 14:44:08 +0900

    終了:ステップ10：複数文字のローカル変数 " commit 8a8a35b9241dcbdc39926e5e64f6f22e8174418f Support multi-letter local variables
    Signed-off-by: kuni3933 <57650186+kuni3933@users.noreply.github.com>

--- main.c
+++ main.c
@@ -3,13 +3,22 @@
 int main(int argc, char **argv) {
   if (argc != 2)
     error("%s: invalid number of arguments", argv[0]);
-  
+
   // Tokenize and parse.
   user_input = argv[1];
   token = tokenize();
-  Node *node = program();
+  Program *prog = program();
+
+  // Assign offsets to local variables.
+  int offset = 0;
+  for (Var *var = prog->locals; var; var = var->next) {
+    offset += 8;
+    var->offset = offset;
+  }
+  prog->stack_size = offset;
 
   // Traverse the AST to emit assembly.
-  codegen(node);  
+  codegen(prog);
+
   return 0;
 }
